4276c4304e0ced8bf74c604ee5cad06079eb5aef
ayeshaqadri <qadria392@gmail.com>
2025-04-27 15:25:29 +0500

    comp

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post('http://localhost:5000/api/auth/register', { name, email, password });
+      await axios.post('https://microfinance-backend-production.up.railway.app/api/auth/register', { name, email, password });
       navigate('/login'); // Redirect to login page after successful registration
     } catch (err) {
       setError('Error registering. Please try again.');
